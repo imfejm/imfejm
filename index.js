f32f5e710d82b220f473db05ac8231eef5416cc5
imfejm <4imfejm@gmail.com>
2026-02-23 12:34:16 +0100

    logoS, menu button

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//navigace na klik a krizek z ham
+const tlacitko = document.querySelector("#ham");
+const rozbal = document.querySelector("#menu");
+const odkazy = rozbal.querySelectorAll("a");
+
+tlacitko.addEventListener("click", () => {
+  rozbal.classList.toggle("hidden");
+
+  document.querySelector("#cara1").classList.toggle("caraA");
+  document.querySelector("#cara2").classList.toggle("caraB");
+  document.querySelector("#cara3").classList.toggle("caraC");
+});
+
+// Zavření menu po kliknutí na jakýkoli odkaz
+odkazy.forEach((link) => {
+  link.addEventListener("click", () => {
+    rozbal.classList.add("hidden");
+
+    document.querySelector("#cara1").classList.remove("caraA");
+    document.querySelector("#cara2").classList.remove("caraB");
+    document.querySelector("#cara3").classList.remove("caraC");
+  });
+});
+
 
